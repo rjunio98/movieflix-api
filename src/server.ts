ff24c7fce8da38c9856abb95034f867ed02e8696
rjunio98 <robledorjunior1@gmail.com>
2023-09-23 09:02:06 -0300

    adicionado filtro de filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -101,6 +101,30 @@ app.delete("/movies/:id", async (req, res) => {
   res.status(200).send();
 });
 
+app.get("/movies/:genreName", async (req, res) => {
+  try {
+    const moviesFilteredByGenreName = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true,
+      },
+      where: {
+        genres: {
+          name: {
+            equals: req.params.genreName,
+            mode: "insensitive",
+          },
+        },
+      },
+    });
+    res.status(200).send(moviesFilteredByGenreName);
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Falha ao filtrar filmes por gênero" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Servidor em execução na porta ${port}`);
 });
